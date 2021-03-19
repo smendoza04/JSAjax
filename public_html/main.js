dfaf438368cbfe6f8bf937774b8ceacc0808ed6a
Sophia Mendoza <manalo.mendoza.sophia.patrice@alumnat.copernic.cat>
2021-03-19 17:22:00 +0100

    starting to make the table (done) starting to make the charts (pending)

--- public_html/main.js
+++ public_html/main.js
@@ -1,22 +1,17 @@
-function CreateTableFromJSON() {
+function CreateTable() {
     $.ajax({ url: "http://datos.santander.es/api/rest/datasets/sensores_smart_env_monitoring.json" })
         .done(function (data) {
             console.log("ok");
             console.log(data);
-    })    
+            refresh(data); //TABLE FUNCTION 
+            
+    });  
  }
  
- function refresh(data) {
-     // EXTRACT VALUE FOR HTML HEADER. 
-        // ('Book ID', 'Book Name', 'Category' and 'Price')
-    var col = [];
-        for (var i = 0; i < data.resources.length; i++) {
-            for (var key in data.resources[i]) {
-                if (col.indexOf(key) === -1) {
-                    col.push(key);
-                }
-            }
-        }    
+ function refresh(data) { //TABLE
+    // EXTRACT VALUE FOR HTML HEADER. 
+    var title = ["Type", "Noise", "Battery", "Latitude", "Temperature", 
+        "Last Modified", "Identifier", "Longitude", "Light", "URL"];
     
     // CREATE DYNAMIC TABLE.
     var table = document.createElement("table");
@@ -25,20 +20,26 @@ function CreateTableFromJSON() {
 
     var tr = table.insertRow(-1);                   // TABLE ROW.
 
-    for (var i = 0; i < col.length; i++) {
-        var th = document.createElement("th");      // TABLE HEADER.
-        th.innerHTML = col[i];
-        tr.appendChild(th);
+    for (var i = 0; i < title.length; i++) {
+        if (title[i] != "Battery" && title[i] !== "Longitude" && title[i] !== "Latitude") {
+            var th = document.createElement("th");      // TABLE HEADER.
+            th.innerHTML = title[i];
+            tr.appendChild(th);
+        }
+        
     }
 
     // ADD JSON DATA TO THE TABLE AS ROWS.
-    for (var i = 0; i < myBooks.length; i++) {
+    for (var i = 0; i < data.resources.length; i++) {
 
         tr = table.insertRow(-1);
 
-        for (var j = 0; j < col.length; j++) {
-            var tabCell = tr.insertCell(-1);
-            tabCell.innerHTML = myBooks[i][col[j]];
+        for (var j in data.resources[i]) {
+            
+            if (j !== "ayto:battery" && j !== "ayto:latitude" && j !== "ayto:longitude") {
+                var tabCell = tr.insertCell(-1);
+                tabCell.innerHTML = data.resources[i][j];
+            }
         }
     }
 
@@ -46,10 +47,4 @@ function CreateTableFromJSON() {
     var divContainer = document.getElementById("showData");
     divContainer.innerHTML = "";
     divContainer.appendChild(table);
- }
- 
- 
- 
-
-    
-    
+}  
