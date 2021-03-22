c9a248a21ab174b0dc0e3add496424b9ff9c4e1d
User <User@DESKTOP-08MSHB5>
2021-03-22 22:44:58 +0100

    Graph finish, kill me

--- public_html/main.js
+++ public_html/main.js
@@ -1,21 +1,19 @@
 function CreateTable() {
     $.ajax({ url: "http://datos.santander.es/api/rest/datasets/sensores_smart_env_monitoring.json" })
-        .done(function (data) {
+        .done(
+            function (data) {
             console.log("ok");
             console.log(data);
-            refresh(data); //TABLE FUNCTION 
             
-            // Load the Visualization API and the piechart package.
-    google.charts.load('current', {'packages':['corechart']});
-
-    // Set a callback to run when the Google Visualization API is loaded.
-    google.charts.setOnLoadCallback(drawChart(data));
-
-    });  
+            refresh(data); //TABLE FUNCTION 
+            setData(data);
+            google.charts.load('current', {'packages':['corechart']});
+            google.charts.setOnLoadCallback(drawChart);
+            }
+        );  
 }
  
 function refresh(data) { //TABLE
-
     // EXTRACT VALUE FOR HTML HEADER. 
     var title = ["Type", "Noise", "Battery", "Latitude", "Temperature", 
         "Last Modified", "Identifier", "Longitude", "Light", "URL"];
@@ -55,14 +53,66 @@ function refresh(data) { //TABLE
     divContainer.appendChild(table);
 }  
 
-function drawChart(data) {
+function drawChart() {
+    var data = google.visualization.arrayToDataTable(array);
+    
+        
+        console.log([
+          ['Year', 'Sales', 'Expenses'],
+          ['2004',  -1000,      400],
+          ['2005',  1170.2,      460],
+          ['2006',  -660.0,       1120],
+          ['2007',  1030,      540]
+        ]);
 
-        // Create our data table out of JSON data loaded from server.
-      
-      console.log("test",data);
+    var options = {
+      title: 'Sensores ambientales',
+      curveType: 'function',
+      legend: { position: 'bottom' }
+    };
 
-      // Instantiate and draw our chart, passing in some options.
-      var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
-      chart.draw(data, {width: 400, height: 240});
+    var chart = new google.visualization.LineChart(document.getElementById('chart_div'));
+
+    chart.draw(data, options);
+}
+
+var array = [['Year', 'Temperature', 'Light']];
+
+function setData(data) {
+
+    var json = {};
+    
+    for(var i = 0; i <  data.resources.length; i++) {
+        if (data.resources[i]["ayto:type"] == "WeatherObserved") {
+  
+            var date = data.resources[i]["dc:modified"].substring(0,4)
+            
+            if (date in json) {
+                json[date]["temperature"] += parseFloat(data.resources[i]["ayto:temperature"]);
+                if (isNaN(parseFloat(data.resources[i]["ayto:light"]))) {
+                    json[date]["light"] += 0;
+                }
+                else {
+                    json[date]["light"] += parseFloat(data.resources[i]["ayto:light"]);
+                }
+                json[date]["count"]++;
+                console.log(parseFloat(data.resources[i]["ayto:light"]));
+            }
+            else {
+                json[date] = {};
+                json[date]["temperature"] = 0;
+                json[date]["light"] = 0;
+                json[date]["count"] = 0;
+                
+            } 
+        }     
+    }    
+    
+    for(var i in json) {
+        array[Object.keys(json).indexOf(i) + 1 ] = [i, parseFloat((json[i].temperature / json[i].count).toFixed(2)),
+            parseFloat((json[i].light / json[i].count).toFixed(2))];
     }
+    return array; 
+}
+
 
